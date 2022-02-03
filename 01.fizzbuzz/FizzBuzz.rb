3e4e7df7a588154b5f8add38ae6a0e8df3b369b8
Soshi Ochiai <equipment2241@gmail.com>
2022-02-03 15:46:57 +0900

    second commit

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,18 +1,18 @@
 # 初期値を0にする
-x=0
+x = 0
 
-# xが20になるまで,xに1ずつ足して、条件分岐させるwhile構文を作成する。
-while x<20
+# 「xに1ずつ足して、条件によって表示させる文字を変える」を20回行う。
+20.times do
   # 変数x（初期値==0）に１ずつ足していく
-  x=x+1
+  x = x + 1
   # 3の倍数かつ5の倍数の時、FizzBuzzと表示
-  if x%3==0 && x%5==0
+  if x % 3 == 0 && x % 5 == 0
     puts "FizzBuzz"
   # 5の倍数の時、Buzzと表示
-  elsif x%5==0
+  elsif x % 5 == 0
     puts "Buzz"
   # 3の倍数の時、Fizzと表示
-  elsif x%3==0
+  elsif x % 3 == 0
     puts "Fizz"
   # それ以外はxを表示
   elsif
