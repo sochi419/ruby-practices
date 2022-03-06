ad1e2e1e4f1cce301e561c0ed5b7740ef0df6778
Soshi Ochiai <equipment2241@gmail.com>
2022-03-06 17:55:45 +0900

    third commit

--- 05.ls/ls_sub.rb
+++ 05.ls/ls_sub.rb
@@ -1,32 +1,31 @@
 # frozen_string_literal: true
 
 class Ls
-  def instance
-    @elements = Dir.glob('*')
-    @column = 3
-  end
-
-  def make_arr
-    instance
-    if (@elements.size % @column).zero?
-      frames = @elements.each_slice(@elements.size / @column).to_a
+  def initialize
+    @directory_elements = Dir.glob('*')
+    if (@directory_elements.size % COLUMN).zero?
+      divided_elements = @directory_elements.each_slice(@directory_elements.size / COLUMN).to_a
     else
-      frames = @elements.each_slice(@elements.size / @column + 1).to_a
-      frames.last << nil until frames.first.size == frames.last.size
+      divided_elements = @directory_elements.each_slice(@directory_elements.size / COLUMN + 1).to_a
+      until divided_elements.first.size == divided_elements.last.size
+        # 後述するtransposeメソッドを使うために、要素数が不足している配列に、nilを代入する。
+        divided_elements.last << nil
+      end
     end
-    @frames = frames
+    @divided_elements = divided_elements
   end
 
   def output
-    make_arr
-    reverse = @frames.transpose
-    reverse.each do |x|
+    sorted_elements = @divided_elements.transpose
+    max_length = @directory_elements.max_by(&:length).length
+    sorted_elements.each do |x|
       x.delete(nil)
-      x.map { |n| n << (' ' * (@elements.max_by(&:length).length - n.length)).to_s }
-      print "#{x.join(' ')}\n"
+      x.map { |n| n << (' ' * (max_length - n.length)).to_s }
+      puts x.join(' ').to_s
     end
   end
 end
 
+COLUMN = 3
 ls = Ls.new
 ls.output
