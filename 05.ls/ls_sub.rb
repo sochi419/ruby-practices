fa69526bb276b5f37f06e19fc561e11819dc11b7
Soshi Ochiai <equipment2241@gmail.com>
2022-03-11 11:13:22 +0900

    Fixed the following points 1. Change method name 2. Fix Method "divide_directories"

--- 05.ls/ls_sub.rb
+++ 05.ls/ls_sub.rb
@@ -6,22 +6,21 @@ class Ls
     @directories = Dir.glob('*')
   end
 
-  def divided_directories
+  def divide_directories
     if (@directories.size % COLUMN).zero?
-      divided_directories = @directories.each_slice(@directories.size / COLUMN).to_a
+      divide_directories = @directories.each_slice(@directories.size / COLUMN).to_a
     else
-      divided_directories = @directories.each_slice(@directories.size / COLUMN + 1).to_a
-      until divided_directories.first.size == divided_directories.last.size
+      divide_directories = @directories.each_slice(@directories.size / COLUMN + 1).to_a
+      until divide_directories.first.size == divide_directories.last.size
         # 後述するtransposeメソッドを使うために、要素数が不足している配列に、nilを代入する。
-        divided_directories.last << nil
+        divide_directories.last << nil
       end
     end
-    @divided_directories = divided_directories
+    return divide_directories
   end
 
-  def output
-    divided_directories
-    sorted_directories = @divided_directories.transpose
+  def sort_directories
+    sorted_directories = divide_directories.transpose
     max_length = @directories.max_by(&:length).length
     sorted_directories.each do |x|
       x.delete(nil)
@@ -32,4 +31,4 @@ class Ls
 end
 
 ls = Ls.new
-ls.output
+ls.sort_directories
