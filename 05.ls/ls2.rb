ef520e80e3e1b37d0154861dbe592416f43fb972
Soshi Ochiai <equipment2241@gmail.com>
2022-04-04 14:40:23 +0900

    I remade a pull request newly. ・I make modifications so that Dir.glob two times are not carried out ・I change handling of adjustment for the interval between the output results

---
+++ 05.ls/ls2.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+class Ls
+  COLUMN = 3
+  def initialize
+    opt = ARGV.getopts('a')
+    if opt['a']
+      @directories = Dir.glob('*', File::FNM_DOTMATCH) 
+    else 
+      @directories = Dir.glob('*')
+    end
+  end
+
+  def divide_directories
+    if (@directories.size % COLUMN).zero?
+      divide_directories = @directories.each_slice(@directories.size / COLUMN).to_a
+    else
+      divide_directories = @directories.each_slice(@directories.size / COLUMN + 1).to_a
+      until divide_directories.first.size == divide_directories.last.size
+        # 後述するtransposeメソッドを使うために、要素数が不足している配列に、nilを代入する。
+        divide_directories.last << nil
+      end
+    end
+    divide_directories
+  end
+
+  def sort_directories
+    sorted_directories = divide_directories.transpose
+    max_length = @directories.max_by(&:length).length
+    sorted_directories.each do |x|
+      x.each do |y|
+        print "%-#{max_length + 1}s"%( y )
+      end
+      # 改行のためputs
+      puts 
+    end
+  end
+end
+
+ls = Ls.new
+ls.sort_directories
