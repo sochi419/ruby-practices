501557df5f659646295a9124314285a7982e5a29
Soshi Ochiai <equipment2241@gmail.com>
2022-09-20 16:36:27 +0900

    Here are the main fixes: ・Change data structure ・Change of calculation process of total

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,77 +3,52 @@
 require 'optparse'
 
 def main
-  opt = ARGV.getopts('l', 'w', 'c')
+  options = ARGV.getopts('l', 'w', 'c')
 
   if ARGV.empty?
-    data_stdin = build_data_for_stdin
-    output(opt, data_stdin, nil, nil)
+    output(build_data_for_stdin, options)
   else
+    total = { line: 0, word: 0, byte: 0, file: 'total' }
     ARGV.each do |file|
-      data_argv = build_data_for_argv(file)
-      output(opt, data_argv, file, nil)
+      total[:line] += build_data_for_file(file)[:line].to_i
+      total[:word] += build_data_for_file(file)[:word].to_i
+      total[:byte] += build_data_for_file(file)[:byte].to_i
+      output(build_data_for_file(file), options)
     end
-    data_total = build_data_for_total(ARGV)
-    output(opt, data_total, nil, ARGV.size) if ARGV.size > 1
+    output(total, options) if ARGV.size > 1
   end
 end
 
 def build_data_for_stdin
-  inputs = $stdin.readlines
-
-  { line: count_line(inputs.join), word: count_words(inputs.join), byte: count_byte(inputs.join) }
+  inputs = $stdin.readlines.join
+  get_calculate_result(inputs, '')
 end
 
-def build_data_for_argv(file)
+def build_data_for_file(file)
   str = File.read(file)
-
-  { line: count_line(str), word: count_words(str), byte: count_byte(str) }
+  get_calculate_result(str, file)
 end
 
-def build_data_for_total(files)
-  count_line_total = 0
-  count_word_total = 0
-  count_byte_total = 0
-
-  files.each do |file|
-    str = File.read(file)
-    count_line_total += count_line(str)
-    count_word_total += count_words(str)
-    count_byte_total += count_byte(str)
-  end
-  { line: count_line_total, word: count_word_total, byte: count_byte_total }
+def get_calculate_result(str, file)
+  { line: count_line(str), word: count_words(str), byte: count_byte(str), file: file }
 end
 
-def output(opt, data, file, total_number_of_argv)
-  output_calculate_result(data, opt)
-  output_filename_or_total(file, total_number_of_argv)
-end
+def output(data, option)
+  print adjust_space(data[:line]) if option['l'] || lack_option(option)
+  print adjust_space(data[:word]) if option['w'] || lack_option(option)
+  print adjust_space(data[:byte]) if option['c'] || lack_option(option)
 
-def output_calculate_result(data, opt)
-  if !opt['l'] && !opt['w'] && !opt['c']
-    print data[:line].to_s.rjust(8, ' ')
-    print data[:word].to_s.rjust(8, ' ')
-    print data[:byte].to_s.rjust(8, ' ')
-  else
-    print data[:line].to_s.rjust(8, ' ') if opt['l']
-    print data[:word].to_s.rjust(8, ' ') if opt['w']
-    print data[:byte].to_s.rjust(8, ' ') if opt['c']
-  end
-end
-
-def output_filename_or_total(file, total_number_of_argv)
-  if ARGV.empty?
-    puts
-    return
-  elsif total_number_of_argv == ARGV.size
-    print(' ')
-    print 'total'
-  else
-    print(' ')
-    print file
-  end
-
+  print(' ')
+  print data[:file]
   puts
+end
+
+def lack_option(option)
+  !option['l'] && !option['w'] && !option['c']
+end
+
+def adjust_space(data)
+  data.to_s.rjust(8, ' ')
 end
 
 def count_line(file)
