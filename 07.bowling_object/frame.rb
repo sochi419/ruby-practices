8e2c8ee9653d8d37052230cf5b215a088e89dd97
sochi419 <equipment2241@gmail.com>
2023-02-06 19:16:52 +0900

    gameとframeの責務を再考した

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,68 +1,23 @@
-# frozen_string_literal: true
-
-require './shot'
+require "./shot"
 
 class Frame
-  attr_reader :scores
+  attr_reader :first_shot, :second_shot, :third_shot
 
-  def initialize(all_shot)
-    @scores = all_shot
+  def initialize(first_mark, second_mark = nil, third_mark = nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
   end
 
   def score
-    shots = []
-    scores.each do |s|
-      shots << Shot.new(s).score
-      shots << 0 if s == 'X'
-    end
-    each_frame = shots.each_slice(2).to_a
-
-    if shots.size == 20 # 第10フレームが2投の場合
-    else # 第10フレームが3投の場合
-      each_frame[9] = each_frame[9..].flatten
-      each_frame.slice!(10, 11)
-      each_frame[9].delete(0)
-    end
-
-    calculate_each_frame_score(each_frame)
+    first_shot.score + second_shot.score + third_shot.score
   end
 
-  def calculate_each_frame_score(each_frame_shots)
-    each_frame_shots.map.with_index do |shot, i|
-      sum = shot[0] + shot[1] + shot[2].to_i
-      if last_frame?(each_frame_shots, i)
-        sum
-      elsif calculate_double_strike_after_nine_frame(each_frame_shots, shot, i) # 第9フレームでストライク且つ第10フレームもストライク
-        sum + each_frame_shots[i + 1][0] + each_frame_shots[i + 1][1]
-      elsif calculate_double_strike_until_eight_frame(each_frame_shots, shot, i) # 第8フレームまででストライク且つ次フレームもストライク
-        sum + each_frame_shots[i + 1][0] + each_frame_shots[i + 2][0]
-      elsif strike?(shot)
-        sum + each_frame_shots[i + 1][0] + each_frame_shots[i + 1][1]
-      elsif spare?(sum)
-        sum + each_frame_shots[i + 1][0]
-      else
-        sum
-      end
-    end
+  def strike?
+    first_shot.score == 10
   end
 
-  def last_frame?(each_frame_shots, index)
-    each_frame_shots[index + 1].nil?
-  end
-
-  def calculate_double_strike_after_nine_frame(each_frame_shots, shot, index)
-    shot[0] == 10 && each_frame_shots[index + 1][0] == 10 && each_frame_shots[index + 2].nil?
-  end
-
-  def calculate_double_strike_until_eight_frame(each_frame_shots, shot, index)
-    shot[0] == 10 && each_frame_shots[index + 1][0] == 10
-  end
-
-  def strike?(shot)
-    shot[0] == 10
-  end
-
-  def spare?(sum)
-    sum == 10
+  def spare?
+    score == 10
   end
 end
--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -3,13 +3,60 @@
 require './frame'
 
 class Game
-  attr_reader :each_frame_scores
+  attr_reader :scores
 
-  def initialize(scores)
-    @each_frame_scores = Frame.new(scores)
+  def initialize(all_shot)
+    @scores = all_shot
   end
 
   def score
-    each_frame_scores.score.sum
+    shots = []
+    scores.each do |s|
+      shots << Shot.new(s).score
+      shots << 0 if s == 'X'
+    end
+    each_frame = shots.each_slice(2).to_a
+
+    if shots.size == 20 # 第10フレームが2投の場合
+    else # 第10フレームが3投の場合
+      each_frame[9] = each_frame[9..].flatten
+      each_frame.slice!(10, 11)
+      each_frame[9].delete(0)
+    end
+
+    calculate_each_frame_score(each_frame)
+  end
+
+  def calculate_each_frame_score(each_frame_shots)
+    each_frame_scores = each_frame_shots.map.with_index do |shot, i|
+      frame = Frame.new(shot[0], shot[1], shot[2].to_i)
+      sum = frame.score
+      if last_frame?(each_frame_shots, i)
+        sum
+      elsif calculate_double_strike_after_nine_frame(each_frame_shots, shot, i) # 第9フレームでストライク且つ第10フレームもストライク
+        sum + each_frame_shots[i + 1][0] + each_frame_shots[i + 1][1]
+      elsif calculate_double_strike_until_eight_frame(each_frame_shots, shot, i) # 第8フレームまででストライク且つ次フレームもストライク
+        sum + each_frame_shots[i + 1][0] + each_frame_shots[i + 2][0]
+      elsif frame.strike?
+        sum + each_frame_shots[i + 1][0] + each_frame_shots[i + 1][1]
+      elsif frame.spare?
+        sum + each_frame_shots[i + 1][0]
+      else
+        sum
+      end
+    end
+    each_frame_scores.sum
+  end
+
+  def last_frame?(each_frame_shots, index)
+    each_frame_shots[index + 1].nil?
+  end
+
+  def calculate_double_strike_after_nine_frame(each_frame_shots, shot, index)
+    shot[0] == 10 && each_frame_shots[index + 1][0] == 10 && each_frame_shots[index + 2].nil?
+  end
+
+  def calculate_double_strike_until_eight_frame(each_frame_shots, shot, index)
+    shot[0] == 10 && each_frame_shots[index + 1][0] == 10
   end
 end
