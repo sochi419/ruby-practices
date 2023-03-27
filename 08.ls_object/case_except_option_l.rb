11174eb0e7d1c2ff2fbe4fb1ba7c08f038a5e5c4
sochi419 <equipment2241@gmail.com>
2023-03-27 09:51:40 +0900

    mainファイルのクラス呼び出しを1行化、reorderクラスを削除してcase_except_option_lクラスに統合

--- 08.ls_object/case_except_option_l.rb
+++ 08.ls_object/case_except_option_l.rb
@@ -1,15 +1,30 @@
 # frozen_string_literal: true
 
 class CaseExceptOptionL
-  attr_reader :sorted_dir, :max_length
+  attr_reader :files, :column
 
-  def initialize(sorted_dir, max_length)
-    @sorted_dir = sorted_dir
-    @max_length = max_length
+  def initialize(files, column)
+    @files = files
+    @column = column
+  end
+
+  def sort_files(files, column)
+    if (files.size % column).zero?
+      divided_directories = files.each_slice(files.size / column).to_a
+    else
+      divided_directories = files.each_slice(files.size / column + 1).to_a
+      until divided_directories.first.size == divided_directories.last.size
+        # 後述するtransposeメソッドを使うために、要素数が不足している配列に、nilを末尾に追加する。
+        divided_directories.last << nil
+      end
+    end
+    divided_directories.transpose
   end
 
   def output_file
-    sorted_dir.each do |sort_directory|
+    max_length = files.max_by(&:length).length
+
+    sort_files(files, column).each do |sort_directory|
       sort_directory.each do |directory|
         print format("%-#{max_length + 1}s", directory)
       end
--- 08.ls_object/main.rb
+++ 08.ls_object/main.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
-require './reorder'
 require './case_except_option_l'
 require './case_option_l'
 require 'optparse'
+
+COLUMN = 3
 
 option = ARGV.getopts('a', 'r', 'l')
 files = option['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
 files = files.reverse if option['r']
 
-COLUMN = 3
-
 if option['l']
   CaseOptionL.new(files).output_file
 else
-  max_length = files.max_by(&:length).length
-  CaseExceptOptionL.new(ArrangingOrder.new(files, COLUMN).sort_files, max_length).output_file
+  CaseExceptOptionL.new(files, COLUMN).output_file
 end
