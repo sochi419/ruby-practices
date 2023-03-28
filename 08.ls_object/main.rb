2579f75ae9cd24037b50d8c15f78bb5ea4c9e1f1
sochi419 <equipment2241@gmail.com>
2023-03-28 10:22:59 +0900

    ファイル名をsnakecaseに変更。定数COLUMNの配置をshort_formatter.rbに移動

--- 08.ls_object/main.rb
+++ 08.ls_object/main.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-require './LongFormatter'
-require './ShortFormatter'
+require './long_formatter'
+require './short_formatter'
 require 'optparse'
-
-COLUMN = 3
 
 option = ARGV.getopts('a', 'r', 'l')
 files = option['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
@@ -13,5 +11,5 @@ files = files.reverse if option['r']
 if option['l']
   LongFormatter.new(files).output_file
 else
-  ShortFormatter.new(files, COLUMN).output_file
+  ShortFormatter.new(files).output_file
 end
---
+++ 08.ls_object/short_formatter.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class ShortFormatter
+  attr_reader :files, :column
+
+  COLUMN = 3
+
+  def initialize(files)
+    @files = files
+  end
+
+  def output_file
+    max_length = files.max_by(&:length).length
+
+    sort_files(files, COLUMN).each do |sort_directory|
+      sort_directory.each do |directory|
+        print format("%-#{max_length + 1}s", directory)
+      end
+      puts
+    end
+  end
+end
+
+private
+
+def sort_files(files, column)
+  if (files.size % column).zero?
+    divided_directories = files.each_slice(files.size / column).to_a
+  else
+    divided_directories = files.each_slice(files.size / column + 1).to_a
+    until divided_directories.first.size == divided_directories.last.size
+      # 後述するtransposeメソッドを使うために、要素数が不足している配列に、nilを末尾に追加する。
+      divided_directories.last << nil
+    end
+  end
+  divided_directories.transpose
+end
