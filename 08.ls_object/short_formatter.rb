35e764ccd1b2dc7b47766a51abca71000ff05de6
sochi419 <equipment2241@gmail.com>
2023-03-29 12:18:45 +0900

    until → timesを使った処理に変更

--- 08.ls_object/short_formatter.rb
+++ 08.ls_object/short_formatter.rb
@@ -23,8 +23,11 @@ class ShortFormatter
   private
 
   def sort_files(files, column)
-    divided_directories = files.each_slice((files.size.to_f / column).ceil).to_a
-    until divided_directories.first.size == divided_directories.last.size
+    number_of_directory_division = (files.size.to_f / column).ceil
+    divided_directories = files.each_slice(number_of_directory_division).to_a
+
+    difference_number_of_elements = divided_directories.first.size - divided_directories.last.size
+    difference_number_of_elements.times do
       # 後述するtransposeメソッドを使うために、要素数が不足している配列に、nilを末尾に追加する。
       divided_directories.last << nil
     end
